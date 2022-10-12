3f77823a052ee2e7c32b45c9cc7d8d1a8f57778e
juanjose1919 <83190654+juanjose1919@users.noreply.github.com>
2022-10-12 05:12:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle as pk
 
-st.title('hola')
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+st.sidebar.markdown(f'<h1 style="text-align: center; color:#F7D559;font-size:35px;">{"INTRODUCCIÓN"}</h1>', unsafe_allow_html=True)
+st.sidebar.markdown(f'<h1 style="text-align: center; color:#000000;font-size:22px;">{"AQUÍ ESCRIBE"}</h1>', unsafe_allow_html=True)
+
+
+
+st.sidebar.markdown(f'<h1 style="text-align: center ; color:#F4D03F ;font-size:35px;">{"AUTORES:"}</h1>', unsafe_allow_html=True)
+
+st.sidebar.markdown(f'<h1 style="text-align: center ; color:#000000 ;font-size:22px;">{"Valeria Mendez"}</h1>', unsafe_allow_html=True)
+st.sidebar.markdown(f'<h1 style="text-align: center ; color:#000000 ;font-size:22px;">{"Gerardo Pardo"}</h1>', unsafe_allow_html=True)
+st.sidebar.markdown(f'<h1 style="text-align: center ; color:#000000 ;font-size:22px;">{"Juan Bohórquez"}</h1>', unsafe_allow_html=True)
+
+
+
+st.sidebar.markdown(f'<h1 style="text-align: center; color:#F4D03F ;font-size:30px;">{"FACULDAD DE ECONOMÍA"}</h1>', unsafe_allow_html=True)
+
+
+
+st.markdown(f'<h1 style="color:#0E6655  ;font-size:50px;">{"Proyecto 9"}</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="color:#000000  ;font-size:25px;">{"En este proyecto se analizará el ingreso de los trabajadores mediante el modelo Random Forest, logrando así obtener un modelo que logre predecir el salario por medio de las siguientes variables:"}</h1>', unsafe_allow_html=True)
