b69b0b3fa09a08b6227a9aab6ce1a820f10f71c7
juanjose1919 <83190654+juanjose1919@users.noreply.github.com>
2022-10-12 12:27:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -131,8 +131,7 @@ Archivo_2 = open('Archivo_modelo_def','rb')
 lista_1 = pk.load(Archivo_2)
 print(lista_1)
     
+
 if P6120 and  P6430 and P6100 and P6050:
-    prediccion = lista_1.predict(np.array([P6120, ESC, P6430, P6800, P6040, P6100, P6585S4]).reshape(1,7)
-st.write('su salario será de', prediccion[0]) 
-
-
+                                 prediccion = lista_1.predict(np.array([P6120, ESC, P6430, P6800, P6040, P6100, P6585S4]).reshape(1,7))
+                                 st.write('su salario será de', prediccion[0])
